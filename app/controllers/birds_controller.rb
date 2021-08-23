6f62d17fd366853c30086b958e75147af99b18a9
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-08-23 14:13:10 -0600

    Completed: Used multiple methods to select json data.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,17 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by_id(params[:id])
+    if bird
+      render json: bird, only: [:id, :name, :species]
+    else
+      render json:{message: 'Bird not found'}
+    end
   end
 end
+
+# except: or only: can specify hash elements
